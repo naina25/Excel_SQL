33791b289269c7aad22073b6787ee471eddedc7e
Naina.Upadhyay <nanu02d8@gmail.com>
2023-01-04 12:36:44 +0530

    merge conflicts

--- Frontend/src/components/Sheet.jsx
+++ Frontend/src/components/Sheet.jsx
@@ -4,37 +4,37 @@ import SheetData from "./SheetData";
 import SheetsDropdown from "./SheetsDropdown";
 
 const Sheet = ({ source, isReport }) => {
-	const [sheets, setSheets] = useState([]);
-	const [isLoadingSheetNames, setIsLoadingSheetNames] = useState(true);
-	const [isLoadingSheetData, setIsLoadingSheetData] = useState(false);
-	const [selectedSheet, setSelectedSheet] = useState("");
-	return (
-		<div>
-			<SheetsDropdown
-				isLoadingSheetNames={isLoadingSheetNames}
-				setIsLoadingSheetNames={setIsLoadingSheetNames}
-				setIsLoadingSheetData={setIsLoadingSheetData}
-				selectedSheet={selectedSheet}
-				setSelectedSheet={setSelectedSheet}
-				sheets={sheets}
-				setSheets={setSheets}
-				source={source}
-			/>
-			{!isReport ? (
-				!isLoadingSheetNames && (
-					<SheetData
-						isLoadingSheetData={isLoadingSheetData}
-						setIsLoadingSheetData={setIsLoadingSheetData}
-						selectedSheet={selectedSheet}
-						setSelectedSheet={setSelectedSheet}
-						sheets={sheets}
-					/>
-				)
-			) : (
-				<ReportPage selectedSheet={selectedSheet} />
-			)}
-		</div>
-	);
+    const [sheets, setSheets] = useState([]);
+    const [isLoadingSheetNames, setIsLoadingSheetNames] = useState(true);
+    const [isLoadingSheetData, setIsLoadingSheetData] = useState(false);
+    const [selectedSheet, setSelectedSheet] = useState("");
+    return (
+        <div>
+            <SheetsDropdown
+                isLoadingSheetNames={isLoadingSheetNames}
+                setIsLoadingSheetNames={setIsLoadingSheetNames}
+                setIsLoadingSheetData={setIsLoadingSheetData}
+                selectedSheet={selectedSheet}
+                setSelectedSheet={setSelectedSheet}
+                sheets={sheets}
+                setSheets={setSheets}
+                source={source}
+            />
+            {!isReport ? (
+                !isLoadingSheetNames && (
+                    <SheetData
+                        isLoadingSheetData={isLoadingSheetData}
+                        setIsLoadingSheetData={setIsLoadingSheetData}
+                        selectedSheet={selectedSheet}
+                        setSelectedSheet={setSelectedSheet}
+                        sheets={sheets}
+                    />
+                )
+            ) : (
+                <ReportPage selectedSheet={selectedSheet} />
+            )}
+        </div>
+    );
 };
 
 export default Sheet;
