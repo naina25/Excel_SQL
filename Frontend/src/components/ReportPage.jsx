862d3b8f02b9b2b3f470aad4748ad38731b491c6
Naina.Upadhyay <nanu02d8@gmail.com>
2023-01-05 16:57:41 +0530

    update styles and displaying data values on pie chart

--- Frontend/src/components/ReportPage.jsx
+++ Frontend/src/components/ReportPage.jsx
@@ -4,111 +4,114 @@ import { useEffect } from "react";
 import PieChart from "./PieChart";
 
 const ReportPage = ({ selectedSheet }) => {
-	const [colNames, setColNames] = useState();
-	const [firstSelectCol, setFirstSelectCol] = useState();
-	const [distinctArr, setDistinctArr] = useState();
-	const [distinctVal, setDistinctVal] = useState();
-	const [secondSelectCol, setSecondSelectCol] = useState();
+    const [colNames, setColNames] = useState();
+    const [firstSelectCol, setFirstSelectCol] = useState();
+    const [distinctArr, setDistinctArr] = useState();
+    const [distinctVal, setDistinctVal] = useState();
+    const [secondSelectCol, setSecondSelectCol] = useState();
 
-	useEffect(() => {
-		const getColumnNames = () => {
-			axios
-				.get(
-					`https://localhost:7108/api/ExcelData/sqltables/${selectedSheet}`
-				)
-				.then((res) => {
-					setColNames(() => res.data.map((val) => val.name));
-				});
-		};
-		getColumnNames();
-	}, [selectedSheet]);
+    useEffect(() => {
+        const getColumnNames = () => {
+            axios
+                .get(
+                    `https://localhost:7108/api/ExcelData/sqltables/${selectedSheet}`
+                )
+                .then((res) => {
+                    setColNames(() => res.data.map((val) => val.name));
+                });
+        };
+        getColumnNames();
+    }, [selectedSheet]);
 
-	useEffect(() => {
-		colNames && colNames.length && setFirstSelectCol(colNames[1]);
-		colNames && colNames.length && setSecondSelectCol(colNames[2]);
-	}, [colNames]);
+    useEffect(() => {
+        colNames && colNames.length && setFirstSelectCol(colNames[1]);
+        colNames && colNames.length && setSecondSelectCol(colNames[2]);
+    }, [colNames]);
 
-	useEffect(() => {
-		const GetDistinctEntries = () => {
-			console.log(firstSelectCol);
-			axios
-				.get(
-					`https://localhost:7108/api/ExcelData/sqltables/${selectedSheet}/${firstSelectCol}`
-				)
-				.then((res) => {
-					setDistinctArr(res.data);
-					setDistinctVal(res.data[0]);
-				});
-		};
-		firstSelectCol && GetDistinctEntries();
-	}, [colNames, firstSelectCol]);
-	return (
-		<div>
-			GROUP BY:{" "}
-			{colNames && colNames.length && (
-				<>
-					<select
-						value={firstSelectCol}
-						onChange={(e) => {
-							setFirstSelectCol(e.target.value);
-						}}
-					>
-						{colNames &&
-							colNames.map((val, index) => {
-								return (
-									index !== 0 && (
-										<option key={index}>{val}</option>
-									)
-								);
-							})}
-					</select>
-					{distinctVal && (
-						<select
-							value={distinctVal[firstSelectCol]}
-							onChange={(e) => {
-								setDistinctVal(e.target.value);
-							}}
-						>
-							{distinctArr &&
-								distinctArr.map((val, index) => {
-									return (
-										<option key={index}>
-											{val[firstSelectCol]}
-										</option>
-									);
-								})}
-						</select>
-					)}
-					<select
-						value={secondSelectCol}
-						onChange={(e) => {
-							setSecondSelectCol(e.target.value);
-						}}
-					>
-						{colNames &&
-							colNames.map((val, index) => {
-								return (
-									index !== 0 &&
-									val !== firstSelectCol && (
-										<option key={index}>{val}</option>
-									)
-								);
-							})}
-					</select>
-				</>
-			)}
-			{secondSelectCol && distinctVal && (
-				<div>
-					<PieChart
-						selectedSheet={selectedSheet}
-						firstSelectCol={firstSelectCol}
-						secondSelectCol={secondSelectCol}
-						distinctVal={distinctVal}
-					/>
-				</div>
-			)}
-		</div>
-	);
+    useEffect(() => {
+        const GetDistinctEntries = () => {
+            console.log(firstSelectCol);
+            axios
+                .get(
+                    `https://localhost:7108/api/ExcelData/sqltables/${selectedSheet}/${firstSelectCol}`
+                )
+                .then((res) => {
+                    setDistinctArr(res.data);
+                    setDistinctVal(res.data[0]);
+                });
+        };
+        firstSelectCol && GetDistinctEntries();
+    }, [firstSelectCol]);
+    return (
+        <div className="reportPage-div">
+            {colNames && colNames.length && (
+                <>
+                    <select
+                        className="report-dropdown"
+                        value={firstSelectCol}
+                        onChange={(e) => {
+                            setFirstSelectCol(e.target.value);
+                        }}
+                    >
+                        {colNames &&
+                            colNames.map((val, index) => {
+                                return (
+                                    index !== 0 && (
+                                        <option key={index}>{val}</option>
+                                    )
+                                );
+                            })}
+                    </select>
+
+                    {distinctVal && (
+                        <select
+                            className="report-dropdown"
+                            value={distinctVal[firstSelectCol]}
+                            onChange={(e) => {
+                                setDistinctVal(e.target.value);
+                            }}
+                        >
+                            {distinctArr &&
+                                distinctArr.map((val, index) => {
+                                    return (
+                                        <option key={index}>
+                                            {val[firstSelectCol]}
+                                        </option>
+                                    );
+                                })}
+                        </select>
+                    )}
+                    <select
+                        className="report-dropdown"
+                        value={secondSelectCol}
+                        onChange={(e) => {
+                            setSecondSelectCol(e.target.value);
+                        }}
+                    >
+                        {colNames &&
+                            colNames.map((val, index) => {
+                                return (
+                                    index !== 0 &&
+                                    val !== firstSelectCol && (
+                                        <option key={index}>{val}</option>
+                                    )
+                                );
+                            })}
+                    </select>
+                </>
+            )}
+            {secondSelectCol && distinctVal && (
+                <div>
+                    <PieChart
+                        selectedSheet={selectedSheet}
+                        firstSelectCol={firstSelectCol}
+                        secondSelectCol={secondSelectCol}
+                        distinctVal={distinctVal}
+                    />
+                </div>
+            )}
+        </div>
+    );
 };
 
 export default ReportPage;
