10b33a7f5316ab8f192939bc97c25c9659e4632e
Deveshkr-19 <Devesh.Singh@bhavnacorp.com>
2023-01-10 13:30:58 +0530

    Edit styling for search and loader components

--- Frontend/src/components/Sheet.jsx
+++ Frontend/src/components/Sheet.jsx
@@ -1,56 +1,56 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import ReportPage from "./ReportPage";
 import SearchComponent from "./SearchComponent";
 import SheetData from "./SheetData";
 import SheetsDropdown from "./SheetsDropdown";
 
 const Sheet = ({ source, isReport }) => {
-    const [sheets, setSheets] = useState([]);
-    const [isLoadingSheetNames, setIsLoadingSheetNames] = useState(true);
-    const [isLoadingSheetData, setIsLoadingSheetData] = useState(false);
-    const [selectedSheet, setSelectedSheet] = useState("");
-    const [sheetData, setSheetData] = useState();
+	const [sheets, setSheets] = useState([]);
+	const [isLoadingSheetNames, setIsLoadingSheetNames] = useState(true);
+	const [isLoadingSheetData, setIsLoadingSheetData] = useState(false);
+	const [selectedSheet, setSelectedSheet] = useState("");
+	const [sheetData, setSheetData] = useState();
 
-    return (
-        <div>
-            <div className="sheetDiv">
-                <SheetsDropdown
-                    isLoadingSheetNames={isLoadingSheetNames}
-                    setIsLoadingSheetNames={setIsLoadingSheetNames}
-                    setIsLoadingSheetData={setIsLoadingSheetData}
-                    selectedSheet={selectedSheet}
-                    setSelectedSheet={setSelectedSheet}
-                    sheets={sheets}
-                    setSheets={setSheets}
-                    source={source}
-                />
-                {!isLoadingSheetNames && (
-                    <SearchComponent
-                        selectedSheet={selectedSheet}
-                        setSheetData={setSheetData}
-                        isLoadingSheetData={isLoadingSheetData}
-                        setIsLoadingSheetData={setIsLoadingSheetData}
-                    />
-                )}
-            </div>
+	return (
+		<div className="content-div">
+			<div className="sheet-div">
+				<SheetsDropdown
+					isLoadingSheetNames={isLoadingSheetNames}
+					setIsLoadingSheetNames={setIsLoadingSheetNames}
+					setIsLoadingSheetData={setIsLoadingSheetData}
+					selectedSheet={selectedSheet}
+					setSelectedSheet={setSelectedSheet}
+					sheets={sheets}
+					setSheets={setSheets}
+					source={source}
+				/>
+				{!isReport && !isLoadingSheetNames && (
+					<SearchComponent
+						selectedSheet={selectedSheet}
+						setSheetData={setSheetData}
+						isLoadingSheetData={isLoadingSheetData}
+						setIsLoadingSheetData={setIsLoadingSheetData}
+					/>
+				)}
+			</div>
 
-            {!isReport ? (
-                !isLoadingSheetNames && (
-                    <SheetData
-                        isLoadingSheetData={isLoadingSheetData}
-                        setIsLoadingSheetData={setIsLoadingSheetData}
-                        selectedSheet={selectedSheet}
-                        setSelectedSheet={setSelectedSheet}
-                        sheets={sheets}
-                        sheetData={sheetData}
-                        setSheetData={setSheetData}
-                    />
-                )
-            ) : (
-                <ReportPage selectedSheet={selectedSheet} />
-            )}
-        </div>
-    );
+			{!isReport ? (
+				!isLoadingSheetNames && (
+					<SheetData
+						isLoadingSheetData={isLoadingSheetData}
+						setIsLoadingSheetData={setIsLoadingSheetData}
+						selectedSheet={selectedSheet}
+						setSelectedSheet={setSelectedSheet}
+						sheets={sheets}
+						sheetData={sheetData}
+						setSheetData={setSheetData}
+					/>
+				)
+			) : (
+				<ReportPage selectedSheet={selectedSheet} />
+			)}
+		</div>
+	);
 };
 
 export default Sheet;
