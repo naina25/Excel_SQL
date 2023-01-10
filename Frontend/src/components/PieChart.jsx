b0d768a1c806f2eddcd36b8938e5f4ff9e9b18f6
Naina.Upadhyay <nanu02d8@gmail.com>
2023-01-10 18:34:00 +0530

    update styles

--- Frontend/src/components/PieChart.jsx
+++ Frontend/src/components/PieChart.jsx
@@ -3,152 +3,152 @@ import React, { useEffect, useState } from "react";
 import Chart from "chart.js/auto";
 
 const PieChart = ({
-	selectedSheet,
-	firstSelectCol,
-	secondSelectCol,
-	selectedArr,
-	GetDistinctEntries,
+    selectedSheet,
+    firstSelectCol,
+    secondSelectCol,
+    selectedArr,
+    GetDistinctEntries,
 }) => {
-	const [chartData, setChartData] = useState();
-	const [distinctArr, setDistinctArr] = useState();
-	const [datasetState, setDatasetState] = useState([]);
+    const [chartData, setChartData] = useState();
+    const [distinctArr, setDistinctArr] = useState();
+    const [datasetState, setDatasetState] = useState([]);
 
-	useEffect(() => {
-		setDistinctArr();
-		GetDistinctEntries(selectedSheet, secondSelectCol, setDistinctArr);
-	}, [firstSelectCol, selectedArr, secondSelectCol]);
+    useEffect(() => {
+        setDistinctArr();
+        GetDistinctEntries(selectedSheet, secondSelectCol, setDistinctArr);
+    }, [firstSelectCol, selectedArr, secondSelectCol]);
 
-	useEffect(() => {
-		setDatasetState([]);
-		if (
-			distinctArr &&
-			chartData &&
-			distinctArr.length &&
-			chartData.length
-		) {
-			for (let i = 0; i < distinctArr.length; i++) {
-				setDatasetState((prev) => [
-					...prev,
-					{
-						label: distinctArr[i][secondSelectCol],
-						data:
-							chartData &&
-							chartData.map((entry) => {
-								if (typeof entry === "string") {
-									const entryObj = JSON.parse(entry);
-									for (let j = 0; j < entryObj.length; j++) {
-										if (
-											entryObj[j][
-												secondSelectCol
-											].trim() ==
-											distinctArr[i][
-												secondSelectCol
-											].trim()
-										) {
-											return entryObj[j].ValCount;
-										} else if (
-											j === entryObj.length - 1 &&
-											entryObj[j][
-												secondSelectCol
-											].trim() !=
-												distinctArr[i][
-													secondSelectCol
-												].trim()
-										) {
-											return 0;
-										}
-									}
-								} else {
-									return entry.ValCount;
-								}
-							}),
-						hoverOffset: 15,
-					},
-				]);
-			}
-		}
-	}, [distinctArr, chartData]);
+    useEffect(() => {
+        setDatasetState([]);
+        if (
+            distinctArr &&
+            chartData &&
+            distinctArr.length &&
+            chartData.length
+        ) {
+            for (let i = 0; i < distinctArr.length; i++) {
+                setDatasetState((prev) => [
+                    ...prev,
+                    {
+                        label: distinctArr[i][secondSelectCol],
+                        data:
+                            chartData &&
+                            chartData.map((entry) => {
+                                if (typeof entry === "string") {
+                                    const entryObj = JSON.parse(entry);
+                                    for (let j = 0; j < entryObj.length; j++) {
+                                        if (
+                                            entryObj[j][
+                                                secondSelectCol
+                                            ].trim() ==
+                                            distinctArr[i][
+                                                secondSelectCol
+                                            ].trim()
+                                        ) {
+                                            return entryObj[j].ValCount;
+                                        } else if (
+                                            j === entryObj.length - 1 &&
+                                            entryObj[j][
+                                                secondSelectCol
+                                            ].trim() !=
+                                                distinctArr[i][
+                                                    secondSelectCol
+                                                ].trim()
+                                        ) {
+                                            return 0;
+                                        }
+                                    }
+                                } else {
+                                    return entry.ValCount;
+                                }
+                            }),
+                        hoverOffset: 15,
+                    },
+                ]);
+            }
+        }
+    }, [distinctArr, chartData]);
 
-	useEffect(() => {
-		const GetChartData = () => {
-			chartData && setChartData();
-			const url =
-				selectedArr.length > 1
-					? `https://localhost:7108/api/ExcelData/sqltables/${selectedSheet}/Barchart`
-					: `https://localhost:7108/api/ExcelData/sqltables/${selectedSheet}/chart`;
-			axios
-				.get(url, {
-					params: {
-						firstCol: firstSelectCol,
-						secondCol: secondSelectCol,
-						selectedVal:
-							selectedArr.length > 1
-								? selectedArr
-								: selectedArr[0],
-					},
-				})
-				.then((res) => {
-					setChartData(JSON.parse(res.data));
-				})
-				.catch(() => setChartData());
-		};
-		selectedArr[0] && GetChartData();
-	}, [firstSelectCol, secondSelectCol, selectedArr, distinctArr]);
+    useEffect(() => {
+        const GetChartData = () => {
+            chartData && setChartData();
+            const url =
+                selectedArr.length > 1
+                    ? `https://localhost:7108/api/ExcelData/sqltables/${selectedSheet}/Barchart`
+                    : `https://localhost:7108/api/ExcelData/sqltables/${selectedSheet}/chart`;
+            axios
+                .get(url, {
+                    params: {
+                        firstCol: firstSelectCol,
+                        secondCol: secondSelectCol,
+                        selectedVal:
+                            selectedArr.length > 1
+                                ? selectedArr
+                                : selectedArr[0],
+                    },
+                })
+                .then((res) => {
+                    setChartData(JSON.parse(res.data));
+                })
+                .catch(() => setChartData());
+        };
+        selectedArr[0] && GetChartData();
+    }, [firstSelectCol, secondSelectCol, selectedArr, distinctArr]);
 
-	useEffect(() => {
-		const ctx = document.getElementById("myChart");
+    useEffect(() => {
+        const ctx = document.getElementById("myChart");
 
-		const chart = new Chart(ctx, {
-			type: selectedArr.length > 1 ? "bar" : "pie",
-			data: {
-				labels:
-					selectedArr && selectedArr.length > 1
-						? selectedArr.map((selectedVal) => {
-								return selectedVal;
-						  })
-						: chartData &&
-						  chartData.map((entry) => {
-								return entry[secondSelectCol];
-						  }),
-				datasets:
-					selectedArr && selectedArr.length > 1
-						? datasetState
-						: [
-								{
-									label: `${secondSelectCol} - ${selectedArr[0]}`,
-									data:
-										chartData &&
-										chartData.map((entry) => {
-											return entry.ValCount;
-										}),
-									backgroundColor:
-										chartData &&
-										chartData.map(() => {
-											var letters =
-												"0123456789ABCDEF".split("");
-											var color = "#";
-											for (var i = 0; i < 6; i++) {
-												color +=
-													letters[
-														Math.floor(
-															Math.random() * 16
-														)
-													];
-											}
-											return color;
-										}),
-									hoverOffset: 15,
-								},
-						  ],
-			},
-		});
+        const chart = new Chart(ctx, {
+            type: selectedArr.length > 1 ? "bar" : "pie",
+            data: {
+                labels:
+                    selectedArr && selectedArr.length > 1
+                        ? selectedArr.map((selectedVal) => {
+                              return selectedVal;
+                          })
+                        : chartData &&
+                          chartData.map((entry) => {
+                              return entry[secondSelectCol];
+                          }),
+                datasets:
+                    selectedArr && selectedArr.length > 1
+                        ? datasetState
+                        : [
+                              {
+                                  label: `${secondSelectCol} - ${selectedArr[0]}`,
+                                  data:
+                                      chartData &&
+                                      chartData.map((entry) => {
+                                          return entry.ValCount;
+                                      }),
+                                  backgroundColor:
+                                      chartData &&
+                                      chartData.map(() => {
+                                          var letters =
+                                              "0123456789ABCDEF".split("");
+                                          var color = "#";
+                                          for (var i = 0; i < 6; i++) {
+                                              color +=
+                                                  letters[
+                                                      Math.floor(
+                                                          Math.random() * 16
+                                                      )
+                                                  ];
+                                          }
+                                          return color;
+                                      }),
+                                  hoverOffset: 15,
+                              },
+                          ],
+            },
+        });
 
-		return () => {
-			chart.destroy();
-		};
-	}, [datasetState]);
+        return () => {
+            chart.destroy();
+        };
+    }, [datasetState]);
 
-	return <canvas id="myChart"></canvas>;
+    return <canvas id="myChart"></canvas>;
 };
 
 export default PieChart;
--- Frontend/src/components/ReportPage.jsx
+++ Frontend/src/components/ReportPage.jsx
@@ -4,157 +4,170 @@ import { useEffect } from "react";
 import PieChart from "./PieChart";
 
 const ReportPage = ({ selectedSheet }) => {
-	const [colNames, setColNames] = useState();
-	const [firstSelectCol, setFirstSelectCol] = useState();
-	const [distinctArr, setDistinctArr] = useState();
-	const [secondSelectCol, setSecondSelectCol] = useState();
-	const [selectedArr, setSelectedArr] = useState();
+    const [colNames, setColNames] = useState();
+    const [firstSelectCol, setFirstSelectCol] = useState();
+    const [distinctArr, setDistinctArr] = useState();
+    const [secondSelectCol, setSecondSelectCol] = useState();
+    const [selectedArr, setSelectedArr] = useState();
 
-	useEffect(() => {
-		const getColumnNames = () => {
-			axios
-				.get(
-					`https://localhost:7108/api/ExcelData/sqltables/${selectedSheet}`
-				)
-				.then((res) => {
-					setColNames(() => res.data.map((val) => val.name));
-				});
-		};
-		selectedSheet && getColumnNames();
-	}, [selectedSheet]);
+    useEffect(() => {
+        const getColumnNames = () => {
+            axios
+                .get(
+                    `https://localhost:7108/api/ExcelData/sqltables/${selectedSheet}`
+                )
+                .then((res) => {
+                    setColNames(() => res.data.map((val) => val.name));
+                });
+        };
+        selectedSheet && getColumnNames();
+    }, [selectedSheet]);
 
-	useEffect(() => {
-		setSelectedArr();
-		colNames && colNames.length && setFirstSelectCol(colNames[1]);
-		colNames && colNames.length && setSecondSelectCol(colNames[2]);
-	}, [colNames]);
+    useEffect(() => {
+        setSelectedArr();
+        colNames && colNames.length && setFirstSelectCol(colNames[1]);
+        colNames && colNames.length && setSecondSelectCol(colNames[2]);
+    }, [colNames]);
 
-	const GetDistinctEntries = (sheet, col, setDistinctArr, setSelectedArr) => {
-		axios
-			.get(
-				`https://localhost:7108/api/ExcelData/sqltables/${sheet}/${col}`
-			)
-			.then((res) => {
-				typeof res.data === "object" && setDistinctArr(res.data);
-				setSelectedArr &&
-					setSelectedArr([
-						res.data[0][col] !== ""
-							? res.data[0][col]
-							: res.data[1][col],
-					]);
-			});
-	};
+    const GetDistinctEntries = (sheet, col, setDistinctArr, setSelectedArr) => {
+        axios
+            .get(
+                `https://localhost:7108/api/ExcelData/sqltables/${sheet}/${col}`
+            )
+            .then((res) => {
+                typeof res.data === "object" && setDistinctArr(res.data);
+                setSelectedArr &&
+                    setSelectedArr([
+                        res.data[0][col] !== ""
+                            ? res.data[0][col]
+                            : res.data[1][col],
+                    ]);
+            });
+    };
 
-	useEffect(() => {
-		firstSelectCol &&
-			GetDistinctEntries(
-				selectedSheet,
-				firstSelectCol,
-				setDistinctArr,
-				setSelectedArr
-			);
-	}, [firstSelectCol]);
+    useEffect(() => {
+        firstSelectCol &&
+            GetDistinctEntries(
+                selectedSheet,
+                firstSelectCol,
+                setDistinctArr,
+                setSelectedArr
+            );
+    }, [firstSelectCol]);
 
-	const SelectCheckbox = (e, val) => {
-		if (e.ctrlKey)
-			if (selectedArr.indexOf(val) !== -1) {
-				setSelectedArr(() => selectedArr.filter((el) => el !== val));
-			} else {
-				setSelectedArr([...selectedArr, val]);
-			}
-		else {
-			setSelectedArr([val]);
-		}
-	};
+    const SelectCheckbox = (e, val) => {
+        if (e.ctrlKey)
+            if (selectedArr.indexOf(val) !== -1) {
+                setSelectedArr(() => selectedArr.filter((el) => el !== val));
+            } else {
+                setSelectedArr([...selectedArr, val]);
+            }
+        else {
+            setSelectedArr([val]);
+        }
+    };
 
-	return (
-		<div className="reportPage-div">
-			{colNames && colNames.length && (
-				<div className="dropdown-div">
-					<select
-						className="report-dropdown"
-						value={firstSelectCol}
-						onChange={(e) => {
-							setSelectedArr();
-							setFirstSelectCol(e.target.value);
-						}}
-					>
-						{colNames &&
-							colNames[0] &&
-							colNames.map((val, index) => {
-								return (
-									index !== 0 && (
-										<option key={index}>{val}</option>
-									)
-								);
-							})}
-					</select>
+    return (
+        <div className="reportPage-div">
+            {colNames && colNames.length && (
+                <div className="dropdown-div">
+                    <label for="col1">
+                        Select Column 1 for visual data representation
+                    </label>
+                    <select
+                        id="col1"
+                        className="report-dropdown"
+                        value={firstSelectCol}
+                        onChange={(e) => {
+                            setSelectedArr();
+                            setFirstSelectCol(e.target.value);
+                        }}
+                    >
+                        {colNames &&
+                            colNames[0] &&
+                            colNames.map((val, index) => {
+                                return (
+                                    index !== 0 && (
+                                        <option key={index}>{val}</option>
+                                    )
+                                );
+                            })}
+                    </select>
+                    <label for="col2">
+                        Select entry/entries for above selected column
+                    </label>
 
-					{selectedArr && (
-						<select
-							className="report-dropdown"
-							// defaultValue={distinctVal[firstSelectCol]}
-							defaultValue={[
-								selectedArr &&
-									selectedArr.length &&
-									selectedArr,
-							]}
-							multiple
-						>
-							{distinctArr &&
-								distinctArr.length > 0 &&
-								distinctArr.map((val, index) => {
-									return (
-										val[firstSelectCol] !== "" && (
-											<option
-												key={index}
-												onClick={(e) =>
-													SelectCheckbox(
-														e,
-														val[firstSelectCol]
-													)
-												}
-											>
-												{val[firstSelectCol]}
-											</option>
-										)
-									);
-								})}
-						</select>
-					)}
-					<select
-						className="report-dropdown"
-						value={secondSelectCol}
-						onChange={(e) => {
-							setSecondSelectCol(e.target.value);
-						}}
-					>
-						{colNames &&
-							colNames.map((val, index) => {
-								return (
-									index !== 0 &&
-									val !== firstSelectCol && (
-										<option key={index}>{val}</option>
-									)
-								);
-							})}
-					</select>
-				</div>
-			)}
-			{secondSelectCol && selectedArr && (
-				<div>
-					<PieChart
-						selectedSheet={selectedSheet}
-						firstSelectCol={firstSelectCol}
-						secondSelectCol={secondSelectCol}
-						selectedArr={selectedArr}
-						setSelectedArr={setSelectedArr}
-						GetDistinctEntries={GetDistinctEntries}
-					/>
-				</div>
-			)}
-		</div>
-	);
+                    {selectedArr && (
+                        <select
+                            id="col2"
+                            className="report-dropdown"
+                            // defaultValue={distinctVal[firstSelectCol]}
+                            defaultValue={[
+                                selectedArr &&
+                                    selectedArr.length &&
+                                    selectedArr,
+                            ]}
+                            multiple
+                        >
+                            {distinctArr &&
+                                distinctArr.length > 0 &&
+                                distinctArr.map((val, index) => {
+                                    return (
+                                        val[firstSelectCol] !== "" && (
+                                            <option
+                                                key={index}
+                                                onClick={(e) =>
+                                                    SelectCheckbox(
+                                                        e,
+                                                        val[firstSelectCol]
+                                                    )
+                                                }
+                                            >
+                                                {val[firstSelectCol]}
+                                            </option>
+                                        )
+                                    );
+                                })}
+                        </select>
+                    )}
+                    <label for="col3">
+                        Select Column 2 for visual data representation
+                    </label>
+
+                    <select
+                        id="col3"
+                        className="report-dropdown"
+                        value={secondSelectCol}
+                        onChange={(e) => {
+                            setSecondSelectCol(e.target.value);
+                        }}
+                    >
+                        {colNames &&
+                            colNames.map((val, index) => {
+                                return (
+                                    index !== 0 &&
+                                    val !== firstSelectCol && (
+                                        <option key={index}>{val}</option>
+                                    )
+                                );
+                            })}
+                    </select>
+                </div>
+            )}
+            {secondSelectCol && selectedArr && (
+                <div>
+                    <PieChart
+                        selectedSheet={selectedSheet}
+                        firstSelectCol={firstSelectCol}
+                        secondSelectCol={secondSelectCol}
+                        selectedArr={selectedArr}
+                        setSelectedArr={setSelectedArr}
+                        GetDistinctEntries={GetDistinctEntries}
+                    />
+                </div>
+            )}
+        </div>
+    );
 };
 
 export default ReportPage;
