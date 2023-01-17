55861e06fd02320217ded8f113eb9153bf8cd3b1
Deveshkr-19 <Devesh.Singh@bhavnacorp.com>
2023-01-17 17:15:34 +0530

    Add error status messages

--- Backend/ExcelSql/ExcelSql/Data/IDaLayer.cs
+++ Backend/ExcelSql/ExcelSql/Data/IDaLayer.cs
@@ -6,11 +6,6 @@ namespace ExcelSql.Data
     public interface IDaLayer
     {
         public List<string> GetSheets();
-        public List<List<string>> GetSheetData(Workbook _workbook, Worksheet ws);
-        public List<List<string>> GetUniqueData(List<List<string>> sheetData, string tableName);
-        public void CreateAndInsert(Workbook _workbook, Worksheet ws);
-        public void InsertData(string tableName, List<List<string>> sheetData);
-        public void AlterTable(string tableName, List<string> colsList);
         public string GetTableData(string tableName);
         public System.Data.DataTable GetSqlTables();
         public string GetTableColumns(string tableName);
@@ -19,7 +14,6 @@ namespace ExcelSql.Data
         public string GetSortedData(string tableName, string column, string order);
         public string GetChartVals(string tableName, string firstCol, string secondCol, string selectedVal);
         public bool IsTablePresent(string tableName);
-        public SimilarTableData IsTableSimilar(Workbook _workbook, Worksheet ws);
         public bool IsColumnPresent(string tableName, string colName);
         public bool IsValuePresent(string tableName, string colName, string val);
 
